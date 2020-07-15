fdf89ea92b944612071cb63dccaa14384afba88a
Erwan31 <spilmonterwan@gmail.com>
2020-07-15 18:31:38 +0100

    Record position with timestamp with file system

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const fs = require('fs');
+
 let record = [];
 
 app.listen(3000, () => console.log('listening at 3000'));
@@ -11,15 +13,32 @@ app.post('/api', (request, response) => {
     console.log('I have a request ');
     const data = request.body;
 
+    // Store data locally
     const rec = [data.lat, data.lon];
-
+    // Save into a file with timestamp
+    const currentDate = new Date();
+    const date = currentDate.getDate();
+    const stringFile = "";
+    
+    // Display an answer onto the client
     response.json({
         status: 'success',
         latitude: request.lat,
         longitude: request.lon,
     });
  
-   record.push( rec );
+    // Keep the position
+   record.push( stringFile.concat(currentDate,",", rec, "\r\n") ); 
  
+   // See the recorded values inside the server console
     console.log('record', record);
-})
+
+    fs.appendFile("/test.txt", record, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    }); 
+}); 
+
+
