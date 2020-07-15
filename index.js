9725eb5748295ec141e258c7a3d8b04c6ab99ce2
Erwan31 <spilmonterwan@gmail.com>
2020-07-15 15:09:47 +0100

    Post data location to server

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const app = express();
 
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb'}));
+
+app.post('/api', (request, response) => {
+    console.log('I have a request ');
+    console.log(request.body);
+
+    response.json({
+        status: 'success',
+        latitude: request.lat,
+        longitude: request.lon,
+    });
+})
