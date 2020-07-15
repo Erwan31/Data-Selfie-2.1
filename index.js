3c0ef9543c8be53d085d907f61afff7777971627
Erwan31 <spilmonterwan@gmail.com>
2020-07-15 18:04:25 +0100

    Simple recording of the client position

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+let record = [];
 
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
@@ -8,11 +9,17 @@ app.use(express.json({ limit: '1mb'}));
 
 app.post('/api', (request, response) => {
     console.log('I have a request ');
-    console.log(request.body);
+    const data = request.body;
+
+    const rec = [data.lat, data.lon];
 
     response.json({
         status: 'success',
         latitude: request.lat,
         longitude: request.lon,
     });
+ 
+   record.push( rec );
+ 
+    console.log('record', record);
 })
